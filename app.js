ba0f29a9bafbc941df83f9bd93d16ee3978e6ba4
Arish Arsalan <159803393+Arish-Arsalan@users.noreply.github.com>
2024-04-04 05:54:00 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+var alien_color = "green";
+if (alien_color == "green") {
+    console.log("You just earned 5 points for shooting the alien!");
+}
+else {
+    console.log("You just earned 10 points.");
+}
+alien_color = "yellow";
+if (alien_color == "green") {
+    console.log("You just earned 5 points for shooting the alien!");
+}
+else {
+    console.log("You just earned 10 points.");
+}
